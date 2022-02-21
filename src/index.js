e55a46b93987745a6475e53de31f08567bd02828
MaxKleem <maxkleem@ukr.net>
2022-02-21 17:07:25 +0200

    HW28

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import Student from './Student';
+import Teacher from './Teacher';
+
+let student = new Student('Vic', 28, 'Female', 'football');
+let teacher = new Teacher('ivanov', 42, 'Male', 'js', 'hokey');
+
+console.log(student);
+console.log(teacher);
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [new HtmlWebpackPlugin(), new CleanWebpackPlugin()],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 3000,
+    open: true,
+  },
+  mode: 'development',
+};
